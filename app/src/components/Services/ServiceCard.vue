52fef9ce2a9e367d8567488314acf3eb5c71c155
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-09 15:52:11 +0100

    added text-center

--- app/src/components/Services/ServiceCard.vue
+++ app/src/components/Services/ServiceCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="col">
     <div class="card h-100">
-      <div class="card-body">
+      <div class="card-body text-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="120"
