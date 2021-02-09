b1c88de05e2252efe57011bf46804e29b2be5333
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-09 16:44:58 +0100

    added image src

--- app/src/components/Services/Services.vue
+++ app/src/components/Services/Services.vue
@@ -19,37 +19,37 @@ export default {
     return {
       services: [
         {
-          img: "http://www.w3.org/2000/svg",
+          img: "appsrcassets\toothSearch.png",
           title: "Tooth inspection",
           body:
             "Comprehensive review of the mouth and detection of broken teeth and other threats.",
         },
         {
-          img: "http://www.w3.org/2000/svg",
+          img: "appsrcassets\toothremove.png",
           title: "Tooth removal",
           body:
             "Any broken tooth is a threat to the body, so this danger should be removed from the mouth.",
         },
         {
-          img: "http://www.w3.org/2000/svg",
+          img: "appsrcassets\removalTartar.png",
           title: "Removal of tartar",
           body:
             "Tartar is the enemy of every human being, because through his presence we have unpleasant breath.",
         },
         {
-          img: "http://www.w3.org/2000/svg",
+          img: "appsrcassetspolishandclean.png",
           title: "Polishing and cleaning",
           body:
             "A simple and safe polishing treatment improves the health of teeth that will get a better look.",
         },
         {
-          img: "http://www.w3.org/2000/svg",
+          img: "../../assets/dentalimplants.png",
           title: "Dental implants",
           body:
             "The best solution when you can not save a healthy tooth and you need to apply artificial solutions.",
         },
         {
-          img: "http://www.w3.org/2000/svg",
+          img: "appsrcassets\toothProtect.png",
           title: "Effective protection",
           body:
             "The cheapest tooth protection is their cleaning and a regular visit to the dentist.",
