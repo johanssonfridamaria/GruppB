cf3ec80780864e50672b96a17bd283c11711d42d
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-09 17:15:54 +0100

    added require to image src

--- app/src/components/Services/Services.vue
+++ app/src/components/Services/Services.vue
@@ -19,37 +19,37 @@ export default {
     return {
       services: [
         {
-          img: "appsrcassets\toothSearch.png",
+          img: require("@/assets/toothSearch.png"),
           title: "Tooth inspection",
           body:
             "Comprehensive review of the mouth and detection of broken teeth and other threats.",
         },
         {
-          img: "appsrcassets\toothremove.png",
+          img: require("@/assets/toothremove.png"),
           title: "Tooth removal",
           body:
             "Any broken tooth is a threat to the body, so this danger should be removed from the mouth.",
         },
         {
-          img: "appsrcassets\removalTartar.png",
+          img: require("@/assets/removalTartar.png"),
           title: "Removal of tartar",
           body:
             "Tartar is the enemy of every human being, because through his presence we have unpleasant breath.",
         },
         {
-          img: "appsrcassetspolishandclean.png",
+          img: require("@/assets/polishandclean.png"),
           title: "Polishing and cleaning",
           body:
             "A simple and safe polishing treatment improves the health of teeth that will get a better look.",
         },
         {
-          img: "../../assets/dentalimplants.png",
+          img: require("@/assets/dentalimplants.png"),
           title: "Dental implants",
           body:
             "The best solution when you can not save a healthy tooth and you need to apply artificial solutions.",
         },
         {
-          img: "appsrcassets\toothProtect.png",
+          img: require("@/assets/toothProtect.png"),
           title: "Effective protection",
           body:
             "The cheapest tooth protection is their cleaning and a regular visit to the dentist.",
