37baa2470063f3d7a356c6611915a0c843398acb
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-09 14:51:15 +0100

    test to add svg and img

--- app/src/components/Services/ServiceCard.vue
+++ app/src/components/Services/ServiceCard.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="card">
     <div class="card-body">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="120"
+        height="120"
+        viewBox="0 0 120 120"
+      >
+        <circle
+          id="icon_background"
+          data-name="icon background"
+          cx="60"
+          cy="60"
+          r="60"
+          fill="#25e6c8"
+        />
+      </svg>
+      <img :src="service.img" alt="" />
       <h5 class="card-title">Card Title {{ service.title }}</h5>
       <p class="card-text">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolor
