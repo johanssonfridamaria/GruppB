523193edff7dc437060eed8780d20e6dd1d2f398
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-09 14:30:45 +0100

    created services page comp

---
+++ app/src/components/Services/Services.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <div class="border-bottom">
+      <h1>Our Services</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
