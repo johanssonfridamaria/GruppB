a6bd64a3d9134672c009928b04cd582295d5bc63
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-09 15:06:12 +0100

    wrapped in col class

--- app/src/components/Services/ServiceCard.vue
+++ app/src/components/Services/ServiceCard.vue
@@ -1,26 +1,28 @@
 <template>
-  <div class="card">
-    <div class="card-body">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="120"
-        height="120"
-        viewBox="0 0 120 120"
-      >
-        <circle
-          id="icon_background"
-          data-name="icon background"
-          cx="60"
-          cy="60"
-          r="60"
-          fill="#25e6c8"
-        />
-      </svg>
-      <img :src="service.img" alt="" />
-      <h5 class="card-title">{{ service.title }}</h5>
-      <p class="card-text">
-        {{ service.body }}
-      </p>
+  <div class="col">
+    <div class="card">
+      <div class="card-body">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="120"
+          height="120"
+          viewBox="0 0 120 120"
+        >
+          <circle
+            id="icon_background"
+            data-name="icon background"
+            cx="60"
+            cy="60"
+            r="60"
+            fill="#25e6c8"
+          />
+        </svg>
+        <img :src="service.img" alt="" />
+        <h5 class="card-title">{{ service.title }}</h5>
+        <p class="card-text">
+          {{ service.body }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
