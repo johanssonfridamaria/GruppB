95afe19f3f09653d5ac92f2e195292c61c62e24d
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2021-02-09 14:42:07 +0100

    created card with title and text

--- app/src/components/Services/ServiceCard.vue
+++ app/src/components/Services/ServiceCard.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>Card</div>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Card Title {{ service.title }}</h5>
+      <p class="card-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolor
+        consequuntur molestias beatae ipsa commodi vero pariatur ullam autem
+        ratione?
+        {{ service.body }}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
