e9a795757f7a6e1d57ab9480ea0931d423c1b0b1
Eilert Bjurek <eilert.bjurek@gmail.com>
2021-02-10 12:21:54 +0100

    Added  boilerplate for Navbar.

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>Hello</div>
+
+  <nav-bar />
+
 </template>
 
 <script>
-export default {
-  name: "App",
-  components: {},
-};
+
+import NavBar from './Components/NavBar';
+export default 
+{name: 'App',
+  components: {
+     NavBar
+      }
+  };
 </script>
 
-<style>
+<style >
 </style>
---
+++ app/src/Components/NavBar.vue
@@ -0,0 +1,55 @@
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <!-- Container wrapper -->
+  <div class="container-fluid">
+    <!-- Toggle button -->
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-mdb-toggle="collapse"
+      data-mdb-target="#navbarRightAlignExample"
+      aria-controls="navbarRightAlignExample"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <i class="fas fa-bars"></i>
+    </button>
+      <a class="navbar-brand" href="#"><img
+          src="https://mdbootstrap.com/img/logo/mdb-transaprent-noshadows.png"
+          height="20"
+          alt=""
+          loading="lazy"
+          style="margin-top: 2px;"
+        /></a>
+    <!-- Collapsible wrapper -->
+    <div class="collapse navbar-collapse" id="navbarRightAlignExample">
+      <!-- Left links -->
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Contact</a>
+        </li>
+      </ul>
+      <!-- Left links -->
+    </div>
+    <!-- Collapsible wrapper -->
+  </div>
+  <!-- Container wrapper -->
+</nav>
+</template>
+
+<script>
+export default { };
+</script>
+
+<style>
+   
+</style>
