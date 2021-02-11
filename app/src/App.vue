56bc37eaa3f2d4194ea97d4ce0ae17c4b3370305
LokaPoka <glasboll@gmailcom>
2021-02-11 10:15:10 +0100

    Showcase commit

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  <div></div>
+  <div>
+    <showcase />
+  </div>
 </template>
 
 <script>
+import Showcase from './components/Showcase'
 export default {
   name: "App",
-  components: {},
-};
+  components: {
+    Showcase
+  }
+}
 </script>
 
 <style>
+
 </style>
