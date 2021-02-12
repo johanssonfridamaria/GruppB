696cbec359448140d156725b0b17e41d397aa59a
Eilert Bjurek <eilert.bjurek@gmail.com>
2021-02-12 10:17:12 +0100

    Justerat färger på iconer

--- app/src/components/topmenu.vue
+++ app/src/components/topmenu.vue
@@ -2,12 +2,12 @@
 
 <nav class="navbar navbar-light bg-light">
   <div class="container">
-    <a class="navbar-brand" href="#"><i class="far fa-clock me-2"></i>Working hours: Monday - Friday: 9:00 - 18:00 </a>
+    <a class="navbar-brand" href="#"><i class="far fa-clock text-info me-2"></i>Working hours: Monday - Friday: 9:00 - 18:00 </a>
     <div class="d-flex">
-    <a class="navbar-brand" href="https://sv-se.facebook.com/"> <i class="fab fa-facebook-f ms-1"></i></a>
-    <a class="navbar-brand" href="https://twitter.com"><i class="fab fa-twitter ms-3"></i></a>
-    <a class="navbar-brand" href="https://google.com"><i class="fab fa-google-plus-g ms-3"></i></a>
-    <a class="navbar-brand" href="https://instagram.com"><i class="fab fa-instagram ms-3"></i></a>
+    <a class="navbar-brand" href="https://sv-se.facebook.com/"> <i class="fab fa-facebook-f text-primary ms-1"></i></a>
+    <a class="navbar-brand" href="https://twitter.com"><i class="fab fa-twitter text-success ms-3"></i></a>
+    <a class="navbar-brand" href="https://google.com"><i class="fab fa-google-plus-g text-primary ms-3"></i></a>
+    <a class="navbar-brand" href="https://instagram.com"><i class="fab fa-instagram text-primary ms-3"></i></a>
     </div>
 
   </div>
